14e8ddf0500cf0679a2758c3a17498991d462ba0
Your Name <you@example.com>
2015-08-06 15:44:13 +0000

    db

--- config/env/development.js
+++ config/env/development.js
@@ -2,3 +2,6 @@ module.exports = {
   db: 'mongodb://0.0.0.0/mean-book',
  sessionSecret: 'developmentSessionSecret'
 };
+
+
+
--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ var config = require('./config'),
     mongoose = require('mongoose');
 
 module.exports = function() {
-  var db = mongoose.connect(config.db);
+  //var db = mongoose.connect(config.db);
+  var db = mongoose.connect(MONGOLAB_URI);
   require('../app/models/user.server.model');
   require('../app/models/article.server.model');
   return db;
